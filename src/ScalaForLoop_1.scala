3ec885111dc8af6cd04611b60c6dba765243efeb
scalakid <alaniit007@gmail.com>
2015-10-21 21:22:05 +1100

    Initial commit

---
+++ src/ScalaForLoop_1.scala
@@ -0,0 +1,13 @@
+/**
+ * Created by ADMINN on 21-Oct-15.
+ * Simple scala FOR LOOP to print numbers from 1 to 10
+ */
+object ScalaForLoop_1 {
+
+  def main(args:Array[String]): Unit =
+  {
+    var i=0;
+    for(i<- 1 to 10)
+      println(i)
+  }
+}
